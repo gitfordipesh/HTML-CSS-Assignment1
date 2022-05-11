b3c24154849d79658bc89897454c7e0dbe7879c8
Dipesh Gupta <dipesh.gupta@w3villa.com>
2022-05-11 13:34:10 +0530

    Modified

--- JS/script.js
+++ JS/script.js
@@ -1,9 +1,25 @@
-/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
-function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "navcon") {
-      x.className += " responsive";
-    } else {
-      x.className = "navcon";
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click",()=>{
+    navMenu.classList.toggle("active");
+})
+// On Scroll Function
+window.addEventListener("scroll", function () {
+    let head = document.querySelector(".nav-menu")       
+    let head2 = document.querySelector("header");
+    if (window.scrollY >= 150) {
+      head2.style.backgroundColor = "#3949ab";
+      head.style.backgroundColor = "#3949ab";
+      head2.style.padding = "0px";
+      head.style.top = "50px";
+      head2.style.transition = "0.3s";
     }
-  }
+    else if (this.window.scrollY <= 150) {
+      head2.style.backgroundColor = "transparent";
+      head.style.backgroundColor = "transparent";
+      head.style.top = "70px";
+      head2.style.padding = "10px 0px";
+    }
+  })
+  
